b625d0793810c8a8f180c104843ba3516b0b720a
Lillian Wright <lillianw653@gmail.com>
2026-05-15 19:30:23 -0600

    add CI workflow

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4YIl8oXwpl_ma9r3bFajn3HihlvKrEoc",
-  authDomain: "fake-store-cdd8c.firebaseapp.com",
-  projectId: "fake-store-cdd8c",
-  storageBucket: "fake-store-cdd8c.firebasestorage.app",
-  messagingSenderId: "552034762250",
-  appId: "1:552034762250:web:3c85295f8a76a3df05a9f3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 
